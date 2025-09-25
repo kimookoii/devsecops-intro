d637d10ba46400d3b594e67207f7e1585e5db01b
zicc <ziconakano00@gmail.com>
2025-09-25 22:11:12 +0700

    Modifikasi

--- test_greeting.py
+++ test_greeting.py
@@ -1,4 +1,4 @@
-from greeting import greeting
+from greeting import say_hello
 
-def test_greeting():
-    assert greeting(“Polibest") == "Hello Polibest,welcome to DevSecOps!"
+def test_say_hello():
+    assert say_hello("Polibest") == "Hello Polibest, welcome to DevSecOps!"
